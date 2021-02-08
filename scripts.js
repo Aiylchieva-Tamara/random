74b570be99ed910a8ed8897502016df45ef89142
Your Name <you@example.com>
2021-02-08 06:52:44 +0600

    added 5-li in ul

--- scripts.js
+++ scripts.js
@@ -5,3 +5,24 @@ h1.style.fontSize = "30"
 document.body.append(h1);
 
 let ul = document.createElement("ul");
+for(let i = 1; i <= 5; i++){
+    let li = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = "Example" + i;
+    // let name = [
+    //     {name:"Random color"},
+    //     {name:"Smaller"},
+    //     {name:"Bigger"},
+           //{name:"+"},
+           //{name:"-"},
+    // ]
+    
+    // for (const fruit of name) {
+    //     let button = document.createElement("button");
+    //     button.innerText = fruit.name ;
+    // }
+    button.id = "example" + i;
+    li.append(button);
+    ul.append(li)
+}
+document.body.append(ul)
